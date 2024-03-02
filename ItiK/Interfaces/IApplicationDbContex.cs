9918730efffb874835629174f35376b227301261
ivan marcos herrero <ivan_marcos_herrero@hotmail.com>
2024-03-02 12:54:33 +0100

    interface para el DBContext

---
+++ ItiK/Interfaces/IApplicationDbContex.cs
@@ -0,0 +1,19 @@
+﻿using Dominio.Models.Shared;
+using Dominio.Models.Store;
+using Dominio.Models.Tickets;
+using Dominio.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aplication.Interfaces
+{
+    public interface IApplicationDbContex
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Ticket> Tickets { get; set; }
+        DbSet<Address> Addresses { get; set; }
+        DbSet<TicketItem> Items { get; set; }
+        DbSet<Store> Commerces { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
