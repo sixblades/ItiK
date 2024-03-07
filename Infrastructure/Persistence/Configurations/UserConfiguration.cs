821fa38be5256a35c3370e5598b03f6448a6c7fc
ivan marcos herrero <ivan_marcos_herrero@hotmail.com>
2024-03-07 21:47:08 +0100

    user configuration

--- Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -10,23 +10,50 @@ namespace Infrastructure.Persistence.Configurations
         //TODO: Terminar esta configuracion
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasKey(x => x.Id);
+            builder.ToTable("User")
+                .HasKey(s => s.Id);
 
-            builder.Property(x => x.Id).HasConversion(
-                userId => userId,
-                value => new Guid());
+            builder.Property(s => s.Id)
+                .HasConversion<string>();
 
             builder.Property(e => e.FiscalType)
-            .HasMaxLength(4)
-            .HasConversion(
+               .HasConversion(
                 v => v.ToString(),
                 v => (FiscalTypeEnum)Enum.Parse(typeof(FiscalTypeEnum), v))
-                .IsUnicode(false);
+               .IsRequired();
 
-            //builder.Property(x => x.FiscalNumber).HasConversion(
-            //    fiscalNumber => fiscalNumber,
-            //    value => FiscalNumber.Create(value)!)
-            //    .HasMaxLength(9);
+            builder.Property(x => x.FiscalNumber)
+                 .HasConversion<string>()
+                 .IsRequired()
+                 .HasMaxLength(9);
+
+            builder.Property(x => x.Name)
+                .HasConversion<string>()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Address.Id)
+                .HasConversion<string>()
+                .HasMaxLength(9);
+
+            builder.OwnsOne(a => a.Address, addressbuilder =>
+            {
+                addressbuilder.HasKey(s => s.Id);
+                addressbuilder.Property(s => s.Id).HasConversion<string>();
+                addressbuilder.Property(s => s.Street).HasConversion<string>();
+                addressbuilder.Property(s => s.Number).HasConversion<string>();
+                addressbuilder.Property(s => s.ZipCode).HasConversion<string>();
+                addressbuilder.Property(s => s.Floor).HasConversion<string>();
+                addressbuilder.Property(s => s.Letter).HasConversion<string>();
+                addressbuilder.Property(s => s.City).HasConversion<string>();
+                addressbuilder.Property(s => s.CountryCode).HasConversion<string>();
+
+
+            });
+
+            builder.Property(x => x.Phone)
+                .HasConversion<string>()
+                .HasMaxLength(9);
+
 
 
         }
